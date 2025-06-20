520f605cbfa9bf013e11039e629a51729fadc7ce
electr1fy0 <electr1fy0@icloud.com>
2025-06-20 16:19:35 +0530

    rs api works but chat can't connect both ends of the app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,27 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use gemini_client_api::gemini::{
+    ask::Gemini,
+    types::request::{Part, SystemInstruction},
+    types::sessions::Session,
+};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+async fn request(query: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let mut session = Session::new(1);
+    let instruction = SystemInstruction::from_str("your name is CatGPT. electr1fy0 made you. behave like a cat in every response, give concise outputs, no yapping, people will die if you use markdown syntax at all. use plain text only.",
+    );
+    let ai = Gemini::new(
+        std::env::var("GEMINI_KEY").expect("key not found"),
+        "gemini-2.5-flash",
+        Some(instruction),
+    );
+
+    let response = ai.ask(session.ask_string(query)).await.unwrap();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
